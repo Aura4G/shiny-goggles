3713d2aa3752f5f481f810249917aed0d8460d60
Aura4G <111776997+Aura4G@users.noreply.github.com>
2023-05-22 21:31:24 +0100

    Add files via upload

---
+++ stackArray test/main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace stackArray_test
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            stack mathsStack = new stack();
+            string response;
+            bool running = true;
+            while (running)
+            {
+                int removed = 0;
+                int total = 0;
+                Console.WriteLine("1) Maths-Stack  2) quit");
+                response = Console.ReadLine();
+                if (response == "1")
+                {
+                    bool returnValue = true;
+                    while (returnValue)
+                    {
+                        Console.Write("Enter number: ");
+                        string response2 = Console.ReadLine();
+                        int newNum = Convert.ToInt32(response2);
+                        returnValue = mathsStack.push(newNum);
+                        if (returnValue == false)
+                        {
+                            Console.WriteLine("Stack full");
+                        }
+                    }
+                    while (mathsStack.pointerValue > 0 && removed < 20)
+                    {
+                        total = total + mathsStack.stackArray[mathsStack.pointerValue -1];
+                        mathsStack.pop();
+                        Console.WriteLine(total);
+                        removed++;
+                    }
+                }
+                if (response == "2")
+                {
+                    running = false;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ stackArray test/stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace stackArray_test
+{
+    public class stack
+    {
+        public int[] stackArray = new int[20];
+        public int pointerValue = 0;
+        int returnValue;
+
+        public int pop()
+        {
+            if (pointerValue == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                pointerValue--;
+                returnValue = stackArray[pointerValue];
+                return returnValue;
+            }
+        }
+
+        public bool push(int value)
+        {
+            if (pointerValue == stackArray.Length)
+            {
+                return false;
+            }
+            else
+            {
+                stackArray[pointerValue] = value;
+                pointerValue++;
+                return true;
+            }
+        }
+
+        public void displayArray()
+        {
+            int counter = 0;
+            while (counter < pointerValue)
+            {
+                Console.Write(stackArray[counter] + ", ");
+                counter++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
